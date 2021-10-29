51b6d98771ca77726a8b2db65c73022677249b46
ErikOstlund <eostlund74@gmail.com>
2021-10-29 11:45:05 -0500

    Initial api call
    to get books based on user's query

--- src/discover.js
+++ src/discover.js
@@ -1,47 +1,58 @@
 /** @jsx jsx */
-import {jsx} from '@emotion/core'
+import { jsx } from '@emotion/core'
 
+import React from 'react'
 import './bootstrap'
 import Tooltip from '@reach/tooltip'
-import {FaSearch} from 'react-icons/fa'
-import {Input, BookListUL, Spinner} from './components/lib'
-import {BookRow} from './components/book-row'
-// 🐨 import the client from './utils/api-client'
+import { FaSearch } from 'react-icons/fa'
+import { Input, BookListUL, Spinner } from './components/lib'
+import { BookRow } from './components/book-row'
+// import { client } from './utils/api-client'
 
 function DiscoverBooksScreen() {
-  // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
-  // 🐨 you'll also notice that we don't want to run the search until the
-  // user has submitted the form, so you'll need a boolean for that as well
-  // 💰 I called it "queried"
+  const [status, setStatus] = React.useState('idle')
+  // query is the user's search input
+  const [query, setQuery] = React.useState('')
+  const [queried, setQueried] = React.useState(false)
+  const [data, setData] = React.useState(null)
 
-  // 🐨 Add a useEffect callback here for making the request with the
-  // client and updating the status and data.
-  // 💰 Here's the endpoint you'll call: `books?query=${encodeURIComponent(query)}`
-  // 🐨 remember, effect callbacks are called on the initial render too
-  // so you'll want to check if the user has submitted the form yet and if
-  // they haven't then return early (💰 this is what the queried state is for).
+  const isLoading = status === 'loading'
+  const isSuccess = status === 'success'
 
-  // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  // make an api call with the query
+  React.useEffect(() => {
+    if (!queried) {
+      return
+    }
+    setStatus('loading')
+    window
+      .fetch(
+        `${process.env.REACT_APP_API_URL}/books?query=${encodeURIComponent(
+          query
+        )}`
+      )
+      .then(response => response.json())
+      .then(responseData => {
+        setData(responseData)
+        setStatus('success')
+      })
+  }, [query, queried])
 
   function handleSearchSubmit(event) {
-    // 🐨 call preventDefault on the event so you don't get a full page reload
-    // 🐨 set the queried state to true
-    // 🐨 set the query value which you can get from event.target.elements
-    // 💰 console.log(event.target.elements) if you're not sure.
+    event.preventDefault()
+    setQuery(event.target.elements.search.value)
+    setQueried(true)
   }
 
   return (
     <div
-      css={{maxWidth: 800, margin: 'auto', width: '90vw', padding: '40px 0'}}
+      css={{ maxWidth: 800, margin: 'auto', width: '90vw', padding: '40px 0' }}
     >
       <form onSubmit={handleSearchSubmit}>
         <Input
           placeholder="Search books..."
           id="search"
-          css={{width: '100%'}}
+          css={{ width: '100%' }}
         />
         <Tooltip label="Search Books">
           <label htmlFor="search">
@@ -51,7 +62,7 @@ function DiscoverBooksScreen() {
                 border: '0',
                 position: 'relative',
                 marginLeft: '-35px',
-                background: 'transparent',
+                background: 'transparent'
               }}
             >
               {isLoading ? <Spinner /> : <FaSearch aria-label="search" />}
@@ -62,7 +73,7 @@ function DiscoverBooksScreen() {
 
       {isSuccess ? (
         data?.books?.length ? (
-          <BookListUL css={{marginTop: 20}}>
+          <BookListUL css={{ marginTop: 20 }}>
             {data.books.map(book => (
               <li key={book.id} aria-label={book.title}>
                 <BookRow key={book.id} book={book} />
@@ -77,4 +88,4 @@ function DiscoverBooksScreen() {
   )
 }
 
-export {DiscoverBooksScreen}
+export { DiscoverBooksScreen }
