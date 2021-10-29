20eb9f423d34911965ab5a43c60bd98a12b9b990
ErikOstlund <eostlund74@gmail.com>
2021-10-28 21:34:13 -0500

    Data Fetch Set-up

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,17 +1,17 @@
 import ReactDOM from 'react-dom'
-import {screen} from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 test('renders the app', () => {
   const root = document.createElement('div')
   root.id = 'root'
   document.body.append(root)
 
-  require('..')
+  require('../app')
 
   screen.getByTitle('Bookshelf')
-  screen.getByRole('heading', {name: /Bookshelf/i})
-  screen.getByRole('button', {name: /Login/i})
-  screen.getByRole('button', {name: /Register/i})
+  screen.getByRole('heading', { name: /Bookshelf/i })
+  screen.getByRole('button', { name: /Login/i })
+  screen.getByRole('button', { name: /Register/i })
 
   // cleanup
   ReactDOM.unmountComponentAtNode(root)
--- src/components/lib.js
+++ src/components/lib.js
@@ -1,54 +1,17 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+
+import { Link as RouterLink } from 'react-router-dom'
 import styled from '@emotion/styled/macro'
 import { keyframes } from '@emotion/core'
-import { Dialog as ReachDialog } from '@reach/dialog'
-import { FaSpinner } from 'react-icons/fa'
 import * as colors from 'styles/colors'
 import * as mq from 'styles/media-queries'
+import { Dialog as ReachDialog } from '@reach/dialog'
+import { FaSpinner } from 'react-icons/fa'
 
 const spin = keyframes({
   '0%': { transform: 'rotate(0deg)' },
   '100%': { transform: 'rotate(360deg)' }
-})
-
-const Spinner = styled(FaSpinner)({
-  animation: `${spin} 1s linear infinite`
-})
-Spinner.defaultProps = {
-  'aria-label': 'loading'
-}
-
-const buttonVariant = {
-  primary: {
-    background: colors.indigo,
-    color: colors.base
-  },
-  secondary: {
-    background: colors.gray,
-    color: colors.text
-  }
-}
-
-const Button = styled.button(
-  {
-    padding: '10px 15px',
-    border: '0',
-    lineHeight: '1',
-    borderRadius: '3px'
-  },
-  // if not variant prop is sent, use 'primary'
-  ({ variant = 'primary' }) => buttonVariant[variant]
-)
-
-const Input = styled.input({
-  borderRadius: '3px',
-  border: `1px solid ${colors.gray10}`,
-  background: colors.gray,
-  padding: '8px 12px'
-})
-
-const FormGroup = styled.div({
-  display: 'flex',
-  flexDirection: 'column'
 })
 
 const CircleButton = styled.button({
@@ -60,11 +23,55 @@ const CircleButton = styled.button({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  background: 'white',
+  background: colors.base,
   color: colors.text,
   border: `1px solid ${colors.gray10}`,
   cursor: 'pointer'
 })
+
+const BookListUL = styled.ul({
+  listStyle: 'none',
+  padding: '0',
+  display: 'grid',
+  gridTemplateRows: 'repeat(auto-fill, minmax(100px, 1fr))',
+  gridGap: '1em'
+})
+
+const Spinner = styled(FaSpinner)({
+  animation: `${spin} 1s linear infinite`
+})
+Spinner.defaultProps = {
+  'aria-label': 'loading'
+}
+
+const buttonVariants = {
+  primary: {
+    background: colors.indigo,
+    color: colors.base
+  },
+  secondary: {
+    background: colors.gray,
+    color: colors.text
+  }
+}
+const Button = styled.button(
+  {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px'
+  },
+  ({ variant = 'primary' }) => buttonVariants[variant]
+)
+
+const inputStyles = {
+  border: '1px solid #f1f1f4',
+  background: '#f1f2f7',
+  padding: '8px 12px'
+}
+
+const Input = styled.input({ borderRadius: '3px' }, inputStyles)
+const Textarea = styled.textarea(inputStyles)
 
 const Dialog = styled(ReachDialog)({
   maxWidth: '450px',
@@ -78,4 +85,91 @@ const Dialog = styled(ReachDialog)({
   }
 })
 
-export { Button, CircleButton, Dialog, Input, FormGroup, Spinner }
+const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column'
+})
+
+function FullPageSpinner() {
+  return (
+    <div
+      css={{
+        fontSize: '4em',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Spinner />
+    </div>
+  )
+}
+
+const Link = styled(RouterLink)({
+  color: colors.indigo,
+  ':hover': {
+    color: colors.indigoDarken10,
+    textDecoration: 'underline'
+  }
+})
+
+const errorMessageVariants = {
+  stacked: { display: 'block' },
+  inline: { display: 'inline-block' }
+}
+
+function ErrorMessage({ error, variant = 'stacked', ...props }) {
+  return (
+    <div
+      role="alert"
+      css={[{ color: colors.danger }, errorMessageVariants[variant]]}
+      {...props}
+    >
+      <span>There was an error: </span>
+      <pre
+        css={[
+          { whiteSpace: 'break-spaces', margin: '0', marginBottom: -5 },
+          errorMessageVariants[variant]
+        ]}
+      >
+        {error.message}
+      </pre>
+    </div>
+  )
+}
+
+function FullPageErrorFallback({ error }) {
+  return (
+    <div
+      role="alert"
+      css={{
+        color: colors.danger,
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <p>Uh oh... There's a problem. Try refreshing the app.</p>
+      <pre>{error.message}</pre>
+    </div>
+  )
+}
+
+export {
+  FullPageErrorFallback,
+  ErrorMessage,
+  CircleButton,
+  BookListUL,
+  Spinner,
+  Button,
+  Input,
+  Textarea,
+  Dialog,
+  FormGroup,
+  FullPageSpinner,
+  Link
+}
