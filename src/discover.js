9b0983bdce0a35cfe07359bee6588352af166333
ErikOstlund <eostlund74@gmail.com>
2021-11-04 17:13:22 -0500

    Refactored window.fetch into reusable util file

--- src/discover.js
+++ src/discover.js
@@ -7,7 +7,7 @@ import Tooltip from '@reach/tooltip'
 import { FaSearch } from 'react-icons/fa'
 import { Input, BookListUL, Spinner } from './components/lib'
 import { BookRow } from './components/book-row'
-// import { client } from './utils/api-client'
+import { client } from './utils/api-client'
 
 function DiscoverBooksScreen() {
   const [status, setStatus] = React.useState('idle')
@@ -25,17 +25,11 @@ function DiscoverBooksScreen() {
       return
     }
     setStatus('loading')
-    window
-      .fetch(
-        `${process.env.REACT_APP_API_URL}/books?query=${encodeURIComponent(
-          query
-        )}`
-      )
-      .then(response => response.json())
-      .then(responseData => {
-        setData(responseData)
-        setStatus('success')
-      })
+    // api call
+    client(`books?query=${encodeURIComponent(query)}`).then(responseData => {
+      setData(responseData)
+      setStatus('success')
+    })
   }, [query, queried])
 
   function handleSearchSubmit(event) {
