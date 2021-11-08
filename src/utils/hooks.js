76631914aea619c679801a290d71ae39b00e9665
ErikOstlund <eostlund74@gmail.com>
2021-11-08 11:52:39 -0600

    Use custom useAsync hook
    Wrap any async calls need in 'run'.

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -8,7 +8,7 @@ function useSafeDispatch(dispatch) {
   }, [])
   return React.useCallback(
     (...args) => (mounted.current ? dispatch(...args) : void 0),
-    [dispatch],
+    [dispatch]
   )
 }
 
@@ -17,40 +17,41 @@ function useSafeDispatch(dispatch) {
 // React.useEffect(() => {
 //   run(fetchPokemon(pokemonName))
 // }, [pokemonName, run])
-const defaultInitialState = {status: 'idle', data: null, error: null}
+const defaultInitialState = { status: 'idle', data: null, error: null }
+
 function useAsync(initialState) {
   const initialStateRef = React.useRef({
     ...defaultInitialState,
-    ...initialState,
+    ...initialState
   })
-  const [{status, data, error}, setState] = React.useReducer(
-    (s, a) => ({...s, ...a}),
-    initialStateRef.current,
+  const [{ status, data, error }, setState] = React.useReducer(
+    (s, a) => ({ ...s, ...a }),
+    initialStateRef.current
   )
 
   const safeSetState = useSafeDispatch(setState)
 
   const setData = React.useCallback(
-    data => safeSetState({data, status: 'resolved'}),
-    [safeSetState],
+    data => safeSetState({ data, status: 'resolved' }),
+    [safeSetState]
   )
   const setError = React.useCallback(
-    error => safeSetState({error, status: 'rejected'}),
-    [safeSetState],
+    error => safeSetState({ error, status: 'rejected' }),
+    [safeSetState]
   )
   const reset = React.useCallback(
     () => safeSetState(initialStateRef.current),
-    [safeSetState],
+    [safeSetState]
   )
 
   const run = React.useCallback(
     promise => {
       if (!promise || !promise.then) {
         throw new Error(
-          `The argument passed to useAsync().run must be a promise. Maybe a function that's passed isn't returning anything?`,
+          `The argument passed to useAsync().run must be a promise. Maybe a function that's passed isn't returning anything?`
         )
       }
-      safeSetState({status: 'pending'})
+      safeSetState({ status: 'pending' })
       return promise.then(
         data => {
           setData(data)
@@ -59,10 +60,10 @@ function useAsync(initialState) {
         error => {
           setError(error)
           return Promise.reject(error)
-        },
+        }
       )
     },
-    [safeSetState, setData, setError],
+    [safeSetState, setData, setError]
   )
 
   return {
@@ -78,8 +79,8 @@ function useAsync(initialState) {
     status,
     data,
     run,
-    reset,
+    reset
   }
 }
 
-export {useAsync}
+export { useAsync }
