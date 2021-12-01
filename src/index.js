fdd42a401a62b2a10037ced82e8d995f16292f28
ErikOstlund <eostlund74@gmail.com>
2021-12-01 16:11:04 -0600

    Custom Profiler

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import { loadDevTools } from './dev-tools/load'
 import './bootstrap'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Profiler } from 'components/profiler'
 import { AppProviders } from 'context'
 import { App } from './app'
 
 loadDevTools(() => {
   ReactDOM.render(
-    <AppProviders>
-      <App />
-    </AppProviders>,
+    <Profiler id="App Root" phases={['mount']}>
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </Profiler>,
     document.getElementById('root')
   )
 })
